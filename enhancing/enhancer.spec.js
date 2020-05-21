a8a2415e4394176b48bba3364ce514901b0d875c
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-21 06:57:56 -0700

    Repair function and test implemented and passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
 // test away!
+//name, durability = 0-100, enhancement = 0-20
+
+test('repair restores durabillity to 100', () => {
+    const reparItem = {name: 'chris', durability: 50, enhancement: 10}
+    const res = enhancer.repair(reparItem)
+    expect(res.durability).toEqual(100)
+})
+
+test('succeed function increments enhancement by if less than 20',)
